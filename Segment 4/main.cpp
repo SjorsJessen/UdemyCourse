6ec4836c76a95bd8490d7acb49f5322bd09ade13
Sjors Jessen <sjors.jessen@gmail.com>
2020-02-23 22:04:30 +0100

    Added minimal Basic IO

--- Segment 4/main.cpp
+++ Segment 4/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
     int favorite_number;
-
+    
     cout << "Enter your favorite number between 1 and 100: ";
     cin  >> favorite_number; 
     cout << "Amazing! That is my favorite number, too!" << endl;
