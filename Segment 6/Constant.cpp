20bbb51502ba9abdc2986f586cefe9715a47a260
Sjors Jessen <sjors.jessen@gmail.com>
2020-02-24 21:25:11 +0100

    Minor Challenge: Constants

--- Segment 6/Constant.cpp
+++ Segment 6/Constant.cpp
@@ -5,26 +5,43 @@ using namespace std;
 int main()
 {
     cout << "Hello, welcome to Frank's Carpet Cleaning Service!" << endl << endl;
-    cout << "How many rooms would you like cleaned? "; 
-
-    int number_of_rooms(0);
-    cin >> number_of_rooms;
+    
+    cout << "How many small rooms would you like cleaned? "; 
+    int number_of_small_rooms(0);
+    cin >> number_of_small_rooms;
     cout << endl;
 
-    const double price_per_room(30.0);
+    cout << "How many large rooms would you like cleaned? "; 
+    int number_of_large_rooms(0);
+    cin >> number_of_large_rooms;
+    cout << endl;
+
+    const double price_per_small_room(25.0);
+    const double price_per_large_room(35.0);
+
     const double sales_tax(0.06);
-    const int estimate_expiry(20);
+    
+    const int estimate_expiry(30);
 
     cout << "Estimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << number_of_rooms << endl;
-    cout << "Price per room: $" << price_per_room << endl;
-    cout << "Cost: $" << price_per_room * number_of_rooms << endl;
-    cout << "Tax: $" << price_per_room * number_of_rooms * sales_tax << endl;
+    cout << "Number of small rooms: " << number_of_small_rooms << endl;
+    cout << "Number of large rooms: " << number_of_large_rooms << endl;
+
+    cout << "Price per small room: $" << price_per_small_room << endl;
+    cout << "Price per large room: $" << price_per_large_room << endl;
+
+    cout << "Cost: $" << price_per_small_room * number_of_small_rooms + price_per_large_room * number_of_large_rooms << endl;
+    cout << "Tax: $" << (price_per_small_room * number_of_small_rooms * sales_tax) + (price_per_large_room * number_of_large_rooms * sales_tax) << endl;
     cout << "===========================================" << endl;
 
-    cout << "Total estimate: $" << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * sales_tax) << endl;
+    cout << "Total estimate: $" 
+    << 
+    ((price_per_small_room * number_of_small_rooms) + (price_per_small_room * number_of_small_rooms * sales_tax)) + 
+    ((price_per_large_room * number_of_large_rooms) + (price_per_large_room * number_of_large_rooms * sales_tax)) 
+    << endl;
+
     cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 
-    cout<< endl;
+    cout << endl;
     return 0;
 }
